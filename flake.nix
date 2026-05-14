d2f3debaad78cac704816212770661c9c94c0191
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-05-14 11:03:11 +0200

    format

--- flake.nix
+++ flake.nix
@@ -11,46 +11,67 @@
       url = "github:BirdeeHub/nix-wrapper-modules";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    plugins-lze = {
-      url = "github:BirdeeHub/lze";
-      flake = false;
-    };
-    plugins-lzextras = {
-      url = "github:BirdeeHub/lzextras";
-      flake = false;
-    };
+    ###
+    # Neovim plugins from outside nixpkgs, either for fetching latest source or
+    # because there is no package yet. See nvim-lib.neovimPlugins in module.nix
+    ###
+    # plugins-lze = {
+    #   url = "github:BirdeeHub/lze";
+    #   flake = false;
+    # };
+    # plugins-lzextras = {
+    #   url = "github:BirdeeHub/lzextras";
+    #   flake = false;
+    # };
   };
 
   outputs =
-    { self, nixpkgs, wrappers, flake-parts, ... }@inputs:
+    {
+      self,
+      nixpkgs,
+      wrappers,
+      flake-parts,
+      ...
+    }@inputs:
+    let
+      lib = nixpkgs.lib;
+    in
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ wrappers.flakeModules.wrappers ];
       systems = nixpkgs.lib.platforms.all;
 
-      perSystem = { pkgs, config, ... }: {
-        _module.args.pkgs = import nixpkgs {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
-        };
-        packages = {
-          full = config.packages.neovim.wrap {
-            settings = {
-              devMode = true;
-              neovide = true;
-              terminalMode = true;
-              unwrappedConfig = "/home/stepii/src/nix/neovim";
+      perSystem =
+        {
+          system,
+          config,
+          ...
+        }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          packages = {
+            # NOTE: reminder that the paths for this package are only used
+            # when running `nix build .#full` for testing
+            full = config.packages.neovim.wrap {
+              settings = {
+                devMode = true;
+                neovide = true;
+                terminalMode = true;
+                unwrappedConfig = "/home/stepii/src/nix/neovim";
+              };
+            };
+            basic = config.packages.neovim.wrap {
+              settings = {
+                neovide = true;
+              };
             };
           };
-          basic = config.packages.neovim.wrap {
-            settings = {
-              neovide = true;
-            };
-          };
         };
-      };
 
       flake.wrappers = {
-        neovim = (import ./module.nix) inputs;
+        neovim = lib.modules.importApply ./module.nix inputs;
         default = self.wrapperModules.neovim;
       };
     };
