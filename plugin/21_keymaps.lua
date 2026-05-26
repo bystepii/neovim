909205ed534fc69373410656272adc52f1473a86
Stepan Klymonchuk <stepanstepan0000@gmail.com>
2026-05-26 12:16:37 +0200

    fix keymap

--- plugin/21_keymaps.lua
+++ plugin/21_keymaps.lua
@@ -58,7 +58,7 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 
 vim.keymap.set('n', '[d', function() vim.diagnostic.jump({count=-1, float=true}) end, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', function() vim.diagnostic.jump({count=1, float=true}) end,  { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float,                           { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float,                           { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist,                           { desc = 'Open diagnostics list' })
 
 --
